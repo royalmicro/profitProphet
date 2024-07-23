8283a7c2749acd42c5e4146cf9104f8f16af8f94
Roger Batista <rbatista@irontec.com>
2024-07-23 21:18:02 +0200

    fixed database connection and migrations

--- app/bin/start.sh
+++ app/bin/start.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+sleep 5
+
 export FLASK_APP=run.py  # Or the entry point of your Flask app
 export FLASK_ENV=development
 export FLASK_RUN_HOST=0.0.0.0
 export FLASK_RUN_PORT=5000
 
-flask run
+python3.12 -m debugpy --wait-for-client --listen 0.0.0.0:5678 -m flask run --host=0.0.0.0
